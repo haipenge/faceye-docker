af8b1a545bae71fd23804c3e723140aae5929c73
haipenge <haipenge@gmail.com>
2017-06-06 01:24:11 +0800

    add middle alisql start scripts

--- docker-ubuntu-16.04-alisql/init-middle-alisql.sh
+++ docker-ubuntu-16.04-alisql/init-middle-alisql.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #For my-middle.cnf
 datadir=/app/data/alisql/data3029
-if [ ! -x "$datadir/data"]; then
-  mkdir $datadir/data
-  mkdir $datadir/mysql
+if [ ! -x "$datadir" ]; then
+  mkdir -p $datadir $datadir/data $datadir/dbs3029 $datadir/mysql
   chown -R mysql:mysql $datadir
   cd /opt/alisql
   mv /etc/my.cnf /tmp
   ./scripts/mysql_install_db --user=mysql --basedir=/opt/alisql --datadir=$datadir/data
-  mv /tmp/my.cnf /etc
-  service mysqld start
+  # mv /tmp/my.cnf /etc
+  # cp /app/data/build/faceye-docker/docker-ubuntu-16.04-alisql/my-middle.cnf /etc/my.cnf
+   cp /app/build/faceye-docker/docker-ubuntu-16.04-alisql/my-middle.cnf /etc/my.cnf
+  # service mysqld start
 else
-  service mysqld start
+  echo '#service mysqld start'
 fi
